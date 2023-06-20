8cd2282a233a83e6538817a4e84cdfcdaea532c3
jeffery <jeffery_9@163.com>
2023-06-21 00:14:40 +0800

    增加搜索功能，去除筛选

--- miniprogram/pages/discuss/index.ts
+++ miniprogram/pages/discuss/index.ts
@@ -1,158 +1,107 @@
-interface SearchItem {
-  type: string;
-  screenKey: string;
-  screenValue: ScreenValue[];
+import publicAPI from "../../api/system/publicAPI";
+import { requestAnimationFrame } from "../../miniprogram_npm/@vant/weapp/common/utils";
+import { chooseFile } from "../../miniprogram_npm/@vant/weapp/uploader/utils";
+const dayjs = require('../../utils/day.min.js');
+interface oneProblem{
+  pid:number,
+  title:string,
+  goods:number,
+  // authorID:number,
+  // Author:{
+  //   isAdmination:string,
+  //   name:string,
+  //   Photo:
+  // },
+  cover_img:string,
+  tag:string[]
+  Picture:string,
 }
 
-interface ScreenValue {
-  checked: boolean;
-  value: string;
-}
+var listAll: oneProblem[][] =  []
 
 Page({
   data: {
-    show: false,
+    value: '',
+    Paging:{
+      size:10,
+      page:1
+    },
     list:[
       {
         pid: 1,
-        cover_img:'https://zhimg.oss-cn-guangzhou.aliyuncs.com/wx/wandiannao_xiao.gif',
+        cover_img:'https://zhimg.oss-cn-guangzhou.aliyuncs.com/wx/021f828776fa53d3dba775fdcff0426b_902397dda144ad340c66a469d2a20cf430ad8506.jpg',
         tag:['11','22'],
         time: '2023.6.1',
       },
-      {
-        pid: 2,
-        cover_img:'https://zhimg.oss-cn-guangzhou.aliyuncs.com/wx/4f4d375e9a9d4f4cff7d26c511609468_e824b899a9014c08c5776e6f0a7b02087af4f4a8.jpg',
-        tag:['11','22','33','44','55'],
-        time: '2023.6.1',
-      }
+      // listAll
     ],
-    searchList: [
-      {
-        type: 'radio',
-        screenKey: '年级',
-        screenValue: ['新订单', '已受理', '已发货', '已过期'].map((m) => ({
-          checked: false,
-          value: m,
-        })),
-      },
-      {
-        type: 'radio',
-        screenKey: '教师',
-        screenValue: ['5天内', '半个月', '一个月内', '一个月上'].map((m) => ({
-          checked: false,
-          value: m,
-        })),
-      },
-      {
-        type: 'checkbox',
-        screenKey: '类型',
-        screenValue: [
-          '配送总部',
-          '信义店',
-          '多又好汉王庙店',
-          '多又好驷马店',
-          '多又好森林公园店',
-          '多又好镇龙店',
-          '西门一店',
-        ].map((m) => ({
-          checked: false,
-          value: m,
-        })),
-      },
-    ],
-  },
-//
-  // onLoad(options) {
-  //   this.query = wx.getStorageSync('query') || [];
-  //   this.getSearchItems();
-  // },
+    listAll
+},
+getAllProblem(Page: Paging){
+  publicAPI.getProblemList(Page).then((res:any)=>{
+    
+    if(res.code===200){
+        // console.log(res)
+        // console.log(res.data)
+        const listAll = this.data.listAll
+        if (res.data.length != 0)  {
+          listAll.push(res.data)
+        }else{
+          return
+        }
+        let page = this.data.Paging.page
+        page++
+        // page-1 [page-1][0-9]  1 10 [0][0-9]
+        
 
-  // 
-  // getSearchItems() {
-  //   const _this = this;
-  //   const searchItems = this.data.searchList.map((n) =>
-  //     Object.assign({}, n, {
-  //       screenValue: n.screenValue.map((m) =>
-  //         Object.assign({}, { checked: _this.query.includes(m.value), value: m.value })
-  //       ),
-  //     })
-  //   );
-  //   this.setData({ searchList: searchItems });
-  // },
-
-
-
-  onChange(e:any) {
-    const { parentIndex, item, index } = e.detail;
-
-    if (item.screenValue[index].checked) {
-      item.screenValue[index].checked = false;
-    } else {
-      if (item.type != 'checkbox') {
-        item.screenValue.map((n:any) => (n.checked = false));
-      }
-      item.screenValue[index].checked = true;
-    }
-
-    this.setData({ [`searchList[${parentIndex}]`]: item }, () => {
-      let selected: any[] = [];
-      this.data.searchList.map((n) => {
-        n.screenValue.map((m) => {
-          if (m.checked == true) {
-            selected.push(m.value);
+        this.setData({
+          listAll:listAll,
+          Paging:{
+            size:10,
+            page:page
           }
-        });
-      });
-      wx.setStorageSync('query', selected);
+        })
+        console.log(listAll);
+    }else if (res.code==-1){
+      wx.showToast({
+        title:"已无更多数据",
+        icon:"error",
+        // errMsg:"已无更多数据"
+      })  
+    }
+  })
+  },
+  goProblemDetail(e:any){
+    // console.log(e);
+    // console.log(e.currentTarget.dataset.id);
+    const id = e.currentTarget.dataset.id;
+    
+    wx.navigateTo({
+      url: '../question/index?id='+id,
+      success(){
+          wx.showToast()
+      },
+      fail(){
+        wx.showToast({
+          icon:"error",
+          title:"失败"
+        })
+        wx.navigateBack
+      }
+    })
+    
+  },
+  //搜索
+  onChange(e:any){
+    this.setData({
+      value: e.detail,
     });
   },
-
-  doCancel() {
-    wx.setStorageSync('query', []);
-    //关闭弹出层
-    this.onClose();
+  onSearch() {
+    console.log(this.data.value);
   },
-  doSubmit() {
-    let selected:any = []
-    // 获取所有选中
-    this.data.searchList.map(n => {
-      n.screenValue.map(m => {
-        if (m.checked == true) {
-          selected.push(m.value)
-        }
-      })
-    })
-    console.log(selected)
-    // var pages = getCurrentPages() // 获取页面栈
-    // var prevPage = pages[pages.length - 2] // 前一个页面
-    // // 携带数据，返回登录之前的页面
-    // prevPage.setData({
-    //   query: selected,
-    //   isBack: true
-    // })
-    // wx.navigateBack({
-    //   delta: 1
-    // })
+  onClick() {
+    console.log(this.data.value);
   },
 
-  reset(){
-    const searchItem = this.data.searchList
-    for (var i = 0; i < searchItem.length; i++) {
-      for (var j = 0; j < searchItem[i].screenValue.length; j++){
-        searchItem[i].screenValue[j].checked=false
-        }
-    }
-    wx.setStorageSync('query', []) 
-    this.setData({
-      searchList: searchItem
-    })
-  },
-  showPopup() {
-    this.setData({ show: true });
-  },
-
-  onClose() {
-    this.setData({ show: false });
-  },
-});
-
+})
