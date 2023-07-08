03566b93c844150f56be1b26e196fb1fea875b5b
jeffery_9 <jeffery_9@163.com>
2023-07-09 01:04:58 +0800

    Merge branch 'dev' into zehua
    替换搜搜为筛选器，优化筛选器

--- miniprogram/pages/discuss/index.ts
+++ miniprogram/pages/discuss/index.ts
@@ -2,6 +2,16 @@ import publicAPI from "../../api/system/publicAPI";
 import { requestAnimationFrame } from "../../miniprogram_npm/@vant/weapp/common/utils";
 import { chooseFile } from "../../miniprogram_npm/@vant/weapp/uploader/utils";
 const dayjs = require('../../utils/day.min.js');
+interface SearchItem {
+  type: string;
+  screenKey: string;
+  screenValue: ScreenValue[];
+}
+
+interface ScreenValue {
+  checked: boolean;
+  value: string;
+}
 interface oneProblem{
   pid:number,
   title:string,
@@ -18,6 +28,32 @@ interface oneProblem{
 }
 
 var listAll: oneProblem[][] =  []
+var searchList: SearchItem[]=[
+  {
+    type: 'radio',
+    screenKey: '年级',
+    screenValue: [].map((m) => ({
+      checked: false,
+      value: m,
+    })),
+  },
+  {
+    type: 'radio',
+    screenKey: '教师',
+    screenValue: [].map((m) => ({
+      checked: false,
+      value: m,
+    })),
+  },
+  {
+    type: 'radio',
+    screenKey: '知识点',
+    screenValue: [].map((m) => ({
+      checked: false,
+      value: m,
+    })),
+  },
+]
 
 Page({
   data: {
@@ -35,7 +71,51 @@ Page({
       },
       // listAll
     ],
-    listAll
+    listAll,
+    searchList
+},
+//设置筛选项
+test(){
+  const searchItem = this.data.searchList;
+  searchItem[0].screenValue=['11'].map((m) => ({
+    checked: false,
+    value: m,
+  }))
+  searchItem[1].screenValue=['22'].map((m) => ({
+    checked: false,
+    value: m,
+  }))
+  searchItem[2].screenValue=['33'].map((m) => ({
+    checked: false,
+    value: m,
+  }))
+  this.setData({
+    searchList: searchItem
+  })
+},
+onChange(e:any) {
+  const { parentIndex, item, index } = e.detail;
+
+  if (item.screenValue[index].checked) {
+    item.screenValue[index].checked = false;
+  } else {
+    if (item.type != 'checkbox') {
+      item.screenValue.map((n:any) => (n.checked = false));
+    }
+    item.screenValue[index].checked = true;
+  }
+
+  this.setData({ [`searchList[${parentIndex}]`]: item }, () => {
+    let selected: any[] = [];
+    this.data.searchList.map((n) => {
+      n.screenValue.map((m) => {
+        if (m.checked == true) {
+          selected.push(m.value);
+        }
+      });
+    });
+    wx.setStorageSync('query', selected);
+  });
 },
 getAllProblem(Page: Paging){
   publicAPI.getProblemList(Page).then((res:any)=>{
@@ -68,6 +148,18 @@ getAllProblem(Page: Paging){
     }
   })
   },
+  doSubmit() {
+    let selected:any = []
+    // 获取所有选中
+    this.data.searchList.map(n => {
+      n.screenValue.map(m => {
+        if (m.checked == true) {
+          selected.push(m.value)
+        }
+      })
+    })
+    console.log(selected)
+  },
   goProblemDetail(e:any){
     const id = e.currentTarget.dataset.id;
     
@@ -86,12 +178,7 @@ getAllProblem(Page: Paging){
     })
     
   },
-  //搜索
-  onChange(e:any){
-    this.setData({
-      value: e.detail,
-    });
-  },
+  
   onSearch() {
   },
   onClick() {
