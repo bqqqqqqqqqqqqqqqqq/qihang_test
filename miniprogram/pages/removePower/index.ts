09c5de29d54fb3395947f0317e40bcac77eabece
jeffery_9 <jeffery_9@163.com>
2023-07-10 22:00:09 +0800

    添加removePower页面

---
+++ miniprogram/pages/removePower/index.ts
@@ -0,0 +1,101 @@
+import Dialog from "../../miniprogram_npm/@vant/weapp/dialog/dialog";
+
+interface user{
+  id: string,
+  name: string
+}
+var userList: user[] = [
+  {
+    id: 'ee',
+    name: 'ff'
+  }
+]
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    type:'',
+    userList
+  },
+  click(e:any){
+    let id = e.currentTarget.dataset.id;
+    let name = e.currentTarget.dataset.name;
+    Dialog.confirm({
+      message: '是否移除'+name+'的权限？',
+    })
+      .then(() => {
+        // on confirm
+        let oldList = this.data.userList;
+        let newList = oldList.filter(item => item.id !== id);
+        this.setData({
+          userList:newList
+        })
+        
+      })
+      .catch(() => {
+        // on cancel
+      });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    let t = options.type;
+    console.log(t);
+    
+    this.setData({
+      type: t
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
