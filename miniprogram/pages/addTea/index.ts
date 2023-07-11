ff92bda4ea28f25472c4e7d2aeb8e3f493405a60
jeffery_9 <jeffery_9@163.com>
2023-07-12 00:47:23 +0800

    复用addTea页面实现添加管理员

--- miniprogram/pages/addTea/index.ts
+++ miniprogram/pages/addTea/index.ts
@@ -11,6 +11,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    type:'',
     userList
   },
   click(e:any){
@@ -32,59 +33,12 @@ Page({
         // on cancel
       });
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {
-
+  onLoad(options) {
+    let t = options.type;
+    console.log(t);
+    
+    this.setData({
+      type: t
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
