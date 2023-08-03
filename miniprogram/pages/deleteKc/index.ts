5243a46e36cfc5e9ecd111898d3789222e28ea19
jeffery_9 <jeffery_9@163.com>
2023-08-03 22:21:13 +0800

    增加页面deleteKc

---
+++ miniprogram/pages/deleteKc/index.ts
@@ -0,0 +1,85 @@
+
+import userApi from "../../api/system/userAPI";
+interface kcInfo {
+  cover_img:string
+  teacherName:string
+  className:string
+  complete:string
+  completeTotal:string
+}
+
+var app = getApp()
+var kcInfo:any[]
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:<any>[],
+  },
+    //跳转
+    go(url:string,params?:string){
+      let gourl = '';
+      if (params) {
+        gourl = '/pages/'+url+'/index?' + params;
+      }else{
+        gourl = '/pages/'+url+'/index';
+      }
+      wx.navigateTo({
+        url:gourl
+    });
+  },
+  delete(e:any){
+    let p = e.currentTarget.dataset.kid;
+   
+    
+  },
+
+  AllKC(){
+    var that = this
+    // const id = that.options.id||""
+    // if (id ==""){
+    //   wx.showToast({
+    //     title:"请重试"
+    //   })
+    //   setTimeout(() => {
+    //     wx.navigateBack()
+    //   }, 500);
+    //   return
+    // }
+     userApi.AllClass({
+           needToken:true,
+           header:{
+          Authorization: app.globalData.token
+        }
+      },"").then((res:any)=>{
+        if(res.code==200&&res.data!=null){
+          console.log(res.data)
+          const list :kcInfo[]=res.data
+          const listAll = this.data.list
+          listAll.push(...list)
+          that.setData({
+            list:listAll
+          })
+      }else if (res.data==null){
+        wx.showToast({
+          title:"已无更多数据",
+          icon:"none"
+        })
+      }else{
+        wx.showToast({
+          title:"已无更多数据",
+          icon:"none"
+        })
+        setTimeout(()=>{
+          wx.navigateBack()
+        },1000)
+        return
+      }
+    })},
+onLoad(){
+  this.AllKC()
+}
+
+})
